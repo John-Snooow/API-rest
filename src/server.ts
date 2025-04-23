1bc5fce65f1c449d64417276c19bb649c0423893
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-22 22:53:24 -0300

    Dados no corpo da requisição
    Aqui eu utilizei o insomnina para recuperar os dados de um produto chamando o nome e o preço dele

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,18 @@ import express from 'express'
 
 const PORT = 3333
 const app = express()
+app.use(express.json())
 
 app.get('/products', (request, response) => {
     const { page, limit } = request.query
 
     response.send(`Página ${page} de ${limit} `)
+})
+
+app.post('/products', (request, response) => {
+    const { name, price } = request.body
+
+    response.send(`O produto ${name} custa ${price} `)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))    
