5c829b9242098270f289dc98cfeb1eb8882f252a
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-22 22:14:41 -0300

    Enviando requisição com o método GET
    é demonstrado como criar a primeira rota e fazer a primeira requisição utilizando o Express. É destacada a simplicidade e facilidade proporcionadas pelo Express em comparação com o Node puro. Mostra-se como definir rotas, acessar requisições e respostas, e como o Express lida automaticamente com rotas inexistentes. A criação de rotas e o envio de respostas utilizando o método send também são abordados. O instrutor destaca a importância de entender o Node puro para apreciar a praticidade do Express.

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,8 @@ import express from 'express'
 const PORT = 3333
 const app = express()
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.get('/', (request, response) => {
+    response.send('Hello World!')
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))    
