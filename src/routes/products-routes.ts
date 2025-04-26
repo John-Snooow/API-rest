1ed13550d9ed306591d9981dee66ccc1a3a43a7c
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-25 22:06:48 -0300

    Controller.
    Neste trecho, são separadas as rotas da aplicação em um arquivo específico e discute-se a criação de um controller para cada rota. É destacada a importância de separar as responsabilidades de roteamento e execução de ações. O instrutor demonstra a criação dos métodos de index e create no controller, explicando a importância de ter acesso à requisição e resposta. Por fim, é mostrado como importar e utilizar o controller nas rotas, mantendo a organização do código.

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -1,20 +1,13 @@
 import { Router } from "express";
 import { myMiddleware } from "../middlewares/my-middleware";
+import { ProductsController } from "../controllers/ProductsController";
 
 const productsRoutes = Router();
+const productsController = new ProductsController()
 
-productsRoutes.get('/', (request, response) => {
-    const { page, limit } = request.query
-
-    response.send(`Página ${page} de ${limit} `)
-})
+productsRoutes.get('/', productsController.index)
 
 //Middleware local em uma rota especifica
-productsRoutes.post('/', myMiddleware, (request, response) => {
-    const { name, price } = request.body
-
-    //response.send(`O produto ${name} custa ${price} `) => Visualizam o objeto em formato de texto
-    response.status(201).json({ name, price, user_id: request.user_id })//vizualiza o mesmo objeto no insomnia
-})
+productsRoutes.post('/', myMiddleware, productsController.create)
 
 export { productsRoutes }
