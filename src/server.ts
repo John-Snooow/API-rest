95debd5b835db152acd4201e7cac7cc88b36199b
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-27 15:47:44 -0300

    AppError
    Neste trecho, expliquei a importância de diferenciar exceções geradas pelo cliente de erros internos do servidor ao lidar com tratamento de exceções em uma aplicação. Destaquei a criação de uma classe AppError para representar esses diferentes tipos de exceções, utilizando mensagens e códigos de status HTTP. Demonstrei como implementar essa classe e como utilizá-la para distinguir e tratar adequadamente as exceções nos controllers da aplicação. Ao final, mostrei a importância de ajustar a lógica no servidor para lidar corretamente com essas exceções.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction} from 'express'
+
 import { routes } from './routes'
+import { AppError } from './utils/AppError'
 
 const PORT = 3333
 
@@ -8,7 +10,17 @@ app.use(express.json())
 
 app.use(routes)
 
+/*
+* 400 (Bad Request): erro do cliente
+* 500 (Internal Server Error): erro interno do servidor
+* 
+*/
+
 app.use((error:any, request: Request, response: Response, _:NextFunction) =>{
+   if(error instanceof AppError){
+    return response.status(error.statusCode).json({message: error.message})
+   }
+    
     response.status(500).json({mesage: error.message})
 })
 
