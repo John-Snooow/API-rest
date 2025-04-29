62ad559e354b2889cae51ddb3b31ccc94bc01d94
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-28 22:32:39 -0300

    Formatando mensagens de erro
    Foi abordado como melhorar a exibição de mensagens de erro ao lidar com validações. Foi destacado o tratamento de exceções, com foco em erros específicos do Zod. Foi mostrado como identificar e tratar erros do Zod, exibindo mensagens personalizadas de erro e detalhes sobre os problemas de validação. A importância de padronizar mensagens de erro e formatar adequadamente as mensagens para uma melhor compreensão também foi enfatizada.

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,34 @@
-import express, { Request, Response, NextFunction} from 'express'
+import express, { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod";
 
-import { routes } from './routes'
-import { AppError } from './utils/app-error'
+import { routes } from "./routes";
+import { AppError } from "./utils/app-error";
 
-const PORT = 3333
+const PORT = 3333;
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
 /*
-* 400 (Bad Request): erro do cliente
-* 500 (Internal Server Error): erro interno do servidor
-* 
-*/
+ * 400 (Bad Request): erro do cliente
+ * 500 (Internal Server Error): erro interno do servidor
+ *
+ */
 
-app.use((error: any, request: Request, response: Response, _:NextFunction) =>{
-   if (error instanceof AppError){
-    return response.status(error.statusCode).json({message: error.message})
-   }
-    
-    response.status(500).json({mesage: error.message})
-})
+app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message });
+  }
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))    
+  if (error instanceof ZodError) {
+    return response
+      .status(400)
+      .json({ message: "Validation error", issues: error.format() });
+  }
+
+  response.status(500).json({ mesage: error.message });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
